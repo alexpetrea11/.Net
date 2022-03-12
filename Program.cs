f0a6a0a0cc624fae260b317dde105a1c1b74af9f
Alexandru Vladut Petrea <101255576+alexpetrea11@users.noreply.github.com>
2022-03-12 22:35:53 +0200

    ConversieGradeCelsiusFahrenheit

--- Program.cs
+++ Program.cs
@@ -1,102 +1,55 @@
 ﻿using System;
 
-namespace ConsoleApp4
+namespace ConsoleApp5
 {
     class Program
     {
-        public static void SimpleSum(float a,float b)
+        public static void ConverttoF(float degrees)
         {
-                      
-            Console.WriteLine(a + " + " + b + " = " + (a + b));
+            degrees = (degrees * 1.8F) + 32;
+            Console.WriteLine("Din Celsius in Fahrenheit: " + degrees.ToString("N3") + "  grade F");
         }
-        public static void SimpleDif(float a,float b)
+        public static void ConverttoC(float degrees)
         {
-            
-         
-            Console.WriteLine(a + " - " + b + " = " + (a - b));
-        }
-        public static void  SimpleDiv(float a,float b)
-        {
-            
-            float rez = 0;
-            try
-            {
-                rez = (float)a / b;
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Operation imposible");
-            }
-            Console.WriteLine(a + " / " + b + " = " + rez);
-        }
-        public static void SimpleMul(float a,float b)
-        {
-         
-            Console.WriteLine(a + " * " + b + " = " + (a * b));
-        }
+            degrees = (degrees - 32) * (0.555F);
+            Console.WriteLine("Din Fahrenheit in Celsius: " + degrees.ToString("N3") + " grade C");
 
+        }
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Calculator  Simplu");
-            float a = 0f;
-              float  b=0f;
-            Console.WriteLine("Alegeti 2 numere cu care urmeaza sa faceti operatii  ");
-            Console.Write("Introduceti numarul a=  ");
+            Console.Write("Alegeti o optiune: ");
+            Console.WriteLine(" Din Fahrenheit in Celsius (C)");
+            Console.WriteLine("Din Celsius in Fahrenheit (F)");
+          
+            char option = Console.ReadKey().KeyChar;
+            Console.Write("\nDati o temperatura de la tastura: ");
+            float degrees = 0;
             try
             {
-                a = float.Parse(Console.ReadLine());
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Wrong type Values");
-            }
-            Console.Write("Introduceti numarul b=  ");
-            try
-            {
-                b = float.Parse(Console.ReadLine());
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Wrong type Values");
-            }
-            char operation='+';
-            Console.WriteLine("Alegeti ce operatie doriti sa efectuati: ");
-            Console.Write("  + - * /  =  ");
-            try
-            {
-                operation = char.Parse(Console.ReadLine());
+                degrees = float.Parse(Console.ReadLine());
             }
             catch (Exception)
             {
                 Console.WriteLine("Wrong type of value given!");
+                option = 'x';
             }
-             switch(operation)
+           
+            switch (option)
             {
-                case '+':
-                    Console.WriteLine("Felicitari! Ati selectat operatia de adunare!");
-                    SimpleSum(a,b);
+                case 'F':
+                    ConverttoF(degrees);
                     break;
-                case '-':
-                    Console.WriteLine("Felicitari! Ati selectat operatia de scadere!");
-                    SimpleDif(a,b);
-                    break;
-                case '/':
-                    Console.WriteLine("Felicitari! Ati selectat operatia de impartire!");
-                    SimpleDiv(a,b);
-                    break;
-                case '*':
-                    Console.WriteLine("Felicitari! Ati selectat operatia de inmultire!");
-                    SimpleMul(a,b);
+                case 'C':
+                    ConverttoC(degrees);
                     break;
                 default:
-                    Console.WriteLine("Invalid operation!");
+                    Console.WriteLine("Optiune Invalida!");
                     break;
             }
             Console.ReadKey();
             Console.Clear();
+
         }
     }
-           
-        }
-    
-
+}
