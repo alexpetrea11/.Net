5f436327dbdbe22cc167d308308ea7f99b99ad75
Alexandru Vladut Petrea <101255576+alexpetrea11@users.noreply.github.com>
2022-03-12 22:33:40 +0200

    CalculatorSimplu

--- Program.cs
+++ Program.cs
@@ -1,48 +1,102 @@
 ﻿using System;
 
-namespace ConsoleApp2
+namespace ConsoleApp4
 {
     class Program
     {
-        public static int FibonacciRecursiv(int n)
+        public static void SimpleSum(float a,float b)
         {
-            if (n == 0)
+                      
+            Console.WriteLine(a + " + " + b + " = " + (a + b));
+        }
+        public static void SimpleDif(float a,float b)
+        {
+            
+         
+            Console.WriteLine(a + " - " + b + " = " + (a - b));
+        }
+        public static void  SimpleDiv(float a,float b)
+        {
+            
+            float rez = 0;
+            try
             {
-                return 0;
+                rez = (float)a / b;
             }
-            else
+            catch (Exception)
             {
-                if (n == 1)
-                {
-                    return 1;
-                }
-                else
-                {
-                    return FibonacciRecursiv(n - 1) + FibonacciRecursiv(n - 2);
-                }
-                public static void SirulluiFibonacci()
-                {
-                    int n = -1;
-                    Console.Write("Mergem pana la numarul n=   ");
-                    try
-                    {
-                        n = int.Parse(Console.ReadLine());
-                    }
-                    catch (Exception)
-                    {
-                        Console.WriteLine("N-ul trebuie sa fie pozitiv");
-                    }
-                    if (n >= 0)
-                    {
-                        Console.WriteLine("F" + n + " = " + FibonacciRecursiv(n));
-                    }
-                    Console.ReadKey();
-                    Console.Clear();
-                }
-                static void Main(string[] args)
+                Console.WriteLine("Operation imposible");
+            }
+            Console.WriteLine(a + " / " + b + " = " + rez);
+        }
+        public static void SimpleMul(float a,float b)
         {
-            Console.WriteLine("Sirul lui Fibonaccu");
-                    SirulluiFibonacci(n);
+         
+            Console.WriteLine(a + " * " + b + " = " + (a * b));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator  Simplu");
+            float a = 0f;
+              float  b=0f;
+            Console.WriteLine("Alegeti 2 numere cu care urmeaza sa faceti operatii  ");
+            Console.Write("Introduceti numarul a=  ");
+            try
+            {
+                a = float.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Wrong type Values");
+            }
+            Console.Write("Introduceti numarul b=  ");
+            try
+            {
+                b = float.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Wrong type Values");
+            }
+            char operation='+';
+            Console.WriteLine("Alegeti ce operatie doriti sa efectuati: ");
+            Console.Write("  + - * /  =  ");
+            try
+            {
+                operation = char.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Wrong type of value given!");
+            }
+             switch(operation)
+            {
+                case '+':
+                    Console.WriteLine("Felicitari! Ati selectat operatia de adunare!");
+                    SimpleSum(a,b);
+                    break;
+                case '-':
+                    Console.WriteLine("Felicitari! Ati selectat operatia de scadere!");
+                    SimpleDif(a,b);
+                    break;
+                case '/':
+                    Console.WriteLine("Felicitari! Ati selectat operatia de impartire!");
+                    SimpleDiv(a,b);
+                    break;
+                case '*':
+                    Console.WriteLine("Felicitari! Ati selectat operatia de inmultire!");
+                    SimpleMul(a,b);
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation!");
+                    break;
+            }
+            Console.ReadKey();
+            Console.Clear();
         }
     }
-}
+           
+        }
+    
+
